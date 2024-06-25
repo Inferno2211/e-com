33a850b062bb6a472398dbb3bbd5d07d0349c171
Inferno2211 <abirjerry2211@gmail.com>
2024-06-25 20:01:02 +0530

    Added frontend

---
+++ frontend/src/Components/Hero/Hero.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Hero.css'
+
+export const Hero = () => {
+    return (
+    <div className='hero'>
+        <div className="hero-left">
+            <h2>New Arrivals Only</h2>
+        </div>
+
+        <div className="hero-right">
+
+        </div>
+    </div>
+    )
+}
